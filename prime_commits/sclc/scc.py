9f339b6e7cdf96a3b4014452eb0594160acd8556
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-03-29 13:12:31 -0500

    Only return the last line of the output table

--- prime_commits/sclc/scc.py
+++ prime_commits/sclc/scc.py
@@ -6,8 +6,12 @@ from pandas import DataFrame
 
 
 def countLines() -> DataFrame:
-    cmdStr: str = "scc -f html-table"
+    cmdStr: str = "scc --no-cocomo -f html-table"
     process: CompletedProcess = subprocess.run(
         args=cmdStr, stdout=subprocess.PIPE, shell=True
     )
-    return pandas.read_html(io=process.stdout)[0]
+    return (
+        pandas.read_html(io=process.stdout, header=0)[0]
+        .iloc[-1:]
+        .reset_index(drop=True)
+    )
