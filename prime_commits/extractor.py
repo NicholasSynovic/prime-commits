8f5a50e4b2e99994249409a08534aaa3f4c82c89
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-01 11:28:30 -0500

    Add steps for a general interface into extracting commits information

---
+++ prime_commits/extractor.py
@@ -0,0 +1,12 @@
+# Steps
+
+# 1. Check if directory exists
+# 2. Check if directory is a repository
+# 3. Check if a branch exists
+# 4. Get the total number of commits in a brach
+# 5. Get an iterator of commits
+# 6. Iterate through iterator and extract commit information
+# 7. Compute any direct metrics
+# 8. Convert types
+# 9. Save to JSON
+# 10. Check JSON conforms to a JSON Schema
