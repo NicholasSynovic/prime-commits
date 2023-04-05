fe87cbe782b82706418411b873799f16a2bcd2e8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-04 19:27:02 -0500

    Move code into the extractor folder

--- prime_commits/extractors/git.py
+++ prime_commits/extractors/git.py
@@ -2,20 +2,22 @@ import logging
 from argparse import Namespace
 from datetime import datetime
 from pathlib import Path
-from sys import exit
 from typing import List
 from warnings import filterwarnings
 
 import pandas
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
 from pandas import DataFrame, Series
 from progress.bar import Bar
 from pygit2 import Commit, Repository
 from pygit2._pygit2 import Walker
 
 from prime_commits.args.extractorArgs import getArgs
-from prime_commits.sclc import scc
+from prime_commits.sclc import cloc, scc
 from prime_commits.utils import filesystem
 from prime_commits.utils.types.commitInformation import CommitInformation
+from prime_commits.utils.types.schema import schema
 from prime_commits.vcs import git
 
 filterwarnings(action="ignore")
@@ -27,23 +29,19 @@ def computeDaysSince0(df: DataFrame, dateColumn: str, daysSince0_Column: str) ->
     df[daysSince0_Column] = pandas.to_timedelta(df[daysSince0_Column]).dt.days
 
 
-def updateDataFrameRowFromSCC(df: DataFrame, sccDF: DataFrame, dfIDX: int) -> None:
-    sccFiles: int = sccDF.loc[0, "Files"]
-    sccLines: int = sccDF.loc[0, "Lines"]
-    sccBlank: int = sccDF.loc[0, "Blank"]
-    sccComment: int = sccDF.loc[0, "Comment"]
-    sccCode: int = sccDF.loc[0, "Code"]
-    sccComplexity: int = sccDF.loc[0, "Complexity"]
-    sccBytes: int = sccDF.loc[0, "Bytes"]
+def updateDataFrameRowFromSCLC(df: DataFrame, sclcDF: DataFrame, dfIDX: int) -> None:
+    sclcFiles: int = sclcDF.loc[0, "Files"]
+    sclcLines: int = sclcDF.loc[0, "Lines"]
+    sclcBlank: int = sclcDF.loc[0, "Blank"]
+    sclcComment: int = sclcDF.loc[0, "Comment"]
+    sclcCode: int = sclcDF.loc[0, "Code"]
 
-    df["NumberOfFiles"].iloc[dfIDX] = sccFiles
-    df["NumberOfLines"].iloc[dfIDX] = sccLines
-    df["NumberOfBlankLines"].iloc[dfIDX] = sccBlank
-    df["NumberOfCommentLines"].iloc[dfIDX] = sccComment
-    df["LOC"].iloc[dfIDX] = sccCode
-    df["KLOC"].iloc[dfIDX] = sccCode / 1000
-    df["SCC_Complexity"].iloc[dfIDX] = sccComplexity
-    df["Bytes"].iloc[dfIDX] = sccBytes
+    df["NumberOfFiles"].iloc[dfIDX] = sclcFiles
+    df["NumberOfLines"].iloc[dfIDX] = sclcLines
+    df["NumberOfBlankLines"].iloc[dfIDX] = sclcBlank
+    df["NumberOfCommentLines"].iloc[dfIDX] = sclcComment
+    df["LOC"].iloc[dfIDX] = sclcCode
+    df["KLOC"].iloc[dfIDX] = sclcCode / 1000
 
 
 def computeDeltas(df: DataFrame, columnName: str, deltaColumnName: str) -> None:
@@ -53,12 +51,26 @@ def computeDeltas(df: DataFrame, columnName: str, deltaColumnName: str) -> None:
 
 def main(args: Namespace) -> None:
     PATH: Path = args.directory
-    BRANCH: str | None = args.branch
+    BRANCH: str | None = args.gitBranch
     OUTPUT: Path = args.output
     LOG: Path = args.log
-    dfList: List[DataFrame] = []
 
-    logging.basicConfig(filename=LOG, datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO)
+    logging.basicConfig(
+        filename=LOG,
+        format="%(asctime)s %(levelname)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    SCLC: int
+    if args.sclc == "scc":
+        SCLC = 0
+        logging.info(msg=f"Using SCC as SCLC")
+    else:
+        SCLC = 1
+        logging.info(msg=f"Using CLOC as SCLC")
+
+    dfList: List[DataFrame] = []
 
     pwd: Path = filesystem.getCWD()
     logging.info(msg=f"Parent working directory is: {pwd}")
@@ -73,11 +85,11 @@ def main(args: Namespace) -> None:
 
     if git.checkIfBranch(branch=BRANCH, repo=repo) is False:
         print(f"Invalid branch name ({BRANCH}) for repository: {PATH}")
-        exit(3)
+        exit(2)
 
     logging.info(msg=f"Using the {BRANCH} branch of {PATH}")
 
-    git.resetHEAD_CMDLINE()
+    git.resetHEAD_CMDLINE(branch=BRANCH)
     commitWalker: Walker = git.getCommitWalker(repo=repo)
     commitCount: int = git.getCommitCount_CMDLINE()
 
@@ -105,17 +117,20 @@ def main(args: Namespace) -> None:
         df=df, dateColumn="AuthorDate", daysSince0_Column="AuthorDaysSince0"
     )
 
-    # TODO: Optimize DataFrame iteration. Vectorization?
-    # TODO: Figure out how to optimally interface with a git repo. FUSE?
     with Bar("Counting lines of code...", max=len(df["id"])) as bar:
         idx: int
         for idx in range(len(df)):
             git.checkoutCommit_CMDLINE(commitID=df["id"].iloc[idx])
-            sccDF: DataFrame = scc.countLines()
-            updateDataFrameRowFromSCC(df=df, sccDF=sccDF, dfIDX=idx)
+
+            if SCLC == 0:
+                sclcDF: DataFrame = scc.countLines()
+            else:
+                sclcDF: DataFrame = cloc.countLines(directory=PATH)
+
+            updateDataFrameRowFromSCLC(df=df, sclcDF=sclcDF, dfIDX=idx)
             bar.next()
 
-    git.resetHEAD_CMDLINE()
+    git.resetHEAD_CMDLINE(branch=BRANCH)
     logging.info(msg=f"Reset {PATH} to HEAD branch")
 
     computeDeltas(df=df, columnName="LOC", deltaColumnName="DLOC")
@@ -125,24 +140,18 @@ def main(args: Namespace) -> None:
     filesystem.switchDirectories(path=pwd)
     logging.info(msg=f"Now working in: {pwd}")
 
+    try:
+        validate(instance=df.T.to_json(), schema=schema)
+    except ValidationError:
+        print(
+            "\n",
+            "ERROR: Unable to validate commits. Please see the log for more information",
+        )
+        logging.info(msg=f"ERROR: Unable to validate JSON: {information.__dict__}")
+        exit(3)
+
     df.T.to_json(
         path_or_buf=OUTPUT,
         indent=4,
     )
     logging.info(msg=f"Saved data to: {OUTPUT}")
-
-
-if __name__ == "__main__":
-    try:
-        args: Namespace = getArgs()
-    except KeyboardInterrupt:
-        exit(1)
-
-    try:
-        main(args=args)
-    except KeyboardInterrupt:
-        pwd: Path = filesystem.getCWD()
-        filesystem.switchDirectories(path=args.directory)
-        git.resetHEAD_CMDLINE()
-        filesystem.switchDirectories(path=pwd)
-        exit(2)
