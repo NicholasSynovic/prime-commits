703dbed91934071cdab9aa2bd8445ed1a06c2c2d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-04 19:30:41 -0500

    Broker the arguement processing prior to extracting commits for a specific VCS

---
+++ prime_commits/extractor.py
@@ -0,0 +1,18 @@
+from argparse import Namespace
+
+from prime_commits.args.extractorArgs import getArgs
+from prime_commits.extractors import git, hg
+
+
+def main() -> None:
+    args: Namespace = getArgs()
+
+    match args.vcs:
+        case "git":
+            git.main(args=args)
+        case "hg":
+            hg.main(args=args)
+
+
+if __name__ == "__main__":
+    main()
