e6090a333c8b1b4a86c408821034dd7dbb328899
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-04 21:08:19 -0500

    Added logging to functions

--- prime_commits/vcs/git.py
+++ prime_commits/vcs/git.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 from subprocess import CompletedProcess
 
@@ -6,14 +7,48 @@ from pygit2 import GIT_SORT_REVERSE, Branch, Repository, Walker
 
 def checkIfBranch(branch: str, repo: Repository) -> bool:
     if repo.lookup_branch(branch) is None:
+        logging.info(msg=f"{branch} is not a valid Git branch")
         return False
+    logging.info(msg=f"{branch} is a valid Git branch")
     return True
 
 
-def getHEADName_CMDLINE() -> str:
+def getDefaultBranchName() -> str:
     cmdStr: str = (
         "git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'"
     )
+    process: CompletedProcess = subprocess.run(
+        args=cmdStr, stdout=subprocess.PIPE, shell=True
+    )
+    branch: str = process.stdout.decode().strip()
+    logging.info(msg=f"{branch} is a valid Git branch")
+    return branch
+
+
+def getCommitCount(branch: str = "HEAD") -> int:
+    cmdStr: str = f"git --no-pager rev-list --count {branch}"
+    process: CompletedProcess = subprocess.run(
+        args=cmdStr, stdout=subprocess.PIPE, shell=True
+    )
+    count: int = int(process.stdout)
+    logging.info(msg=f"Found {count} commits in branch {branch}")
+    return count
+
+
+def checkoutCommit(commitID: str) -> None:
+    cmdStr: str = f"git checkout {commitID} --quiet --force"
+    subprocess.run(args=cmdStr, stdout=subprocess.DEVNULL, shell=True)
+    logging.info(msg=f"Checked out {commitID}")
+
+
+def restoreRepoToBranch(branch: str) -> None:
+    cmdStr: str = f"git checkout {branch} --quiet --force"
+    subprocess.run(args=cmdStr, stdout=subprocess.DEVNULL, shell=True)
+    logging.info(msg=f"Restored repo to {branch} branch")
+
+
+def getCurrentCheckedOutCommit() -> str:
+    cmdStr: str = 'git --no-pager log -1 --pretty="%H"'
     process: CompletedProcess = subprocess.run(
         args=cmdStr, stdout=subprocess.PIPE, shell=True
     )
@@ -22,30 +57,3 @@ def getHEADName_CMDLINE() -> str:
 
 def getCommitWalker(repo: Repository) -> Walker:
     return repo.walk(repo.head.target, GIT_SORT_REVERSE)
-
-
-def getCommitCount_CMDLINE(branch: str = "HEAD") -> int:
-    cmdStr: str = f"git --no-pager rev-list --count {branch}"
-
-    process: CompletedProcess = subprocess.run(
-        args=cmdStr, stdout=subprocess.PIPE, shell=True
-    )
-    return int(process.stdout)
-
-
-def checkoutCommit_CMDLINE(commitID: str) -> None:
-    cmdStr: str = f"git checkout {commitID} --quiet --force"
-    subprocess.run(args=cmdStr, stdout=subprocess.DEVNULL, shell=True)
-
-
-def getCurrentCheckedOutCommit_CMDLINE() -> str:
-    cmdStr: str = 'git --no-pager log -1 --pretty="%H"'
-    process: CompletedProcess = subprocess.run(
-        args=cmdStr, stdout=subprocess.PIPE, shell=True
-    )
-    return process.stdout.decode().strip()
-
-
-def resetHEAD_CMDLINE(branch: str) -> None:
-    cmdStr: str = f"git checkout {branch} --quiet --force"
-    subprocess.run(args=cmdStr, stdout=subprocess.DEVNULL, shell=True)
