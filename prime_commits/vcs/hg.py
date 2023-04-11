773b5fda564c509bb957141f32ce1525efe54bd5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-04-11 09:25:33 -0500

    Create a Hg class

--- prime_commits/vcs/hg.py
+++ prime_commits/vcs/hg.py
@@ -1,45 +1,52 @@
 import logging
 from datetime import datetime
+from pathlib import Path
 from typing import List, Tuple
 
 from hglib.client import hgclient
 
-
-def getDefaultBranchName(repo: hgclient) -> str:
-    branch: str = repo.branch()
-    logging.info(msg=f"{branch} is the default Mercurial branch")
-    return branch
+from prime_commits.vcs.genericVCS import GenericVCS
 
 
-def checkIfBranch(branch: str, repo: hgclient) -> bool:
-    detailedBranches: List[Tuple[bytes, int, bytes]] = repo.branches(closed=True)
-    branches: List[str] = [branch.decode() for branch, _, _ in detailedBranches]
+class Hg(GenericVCS):
+    def __init__(self, repositoryPath: Path) -> None:
+        self.repositoryPath: Path = repositoryPath
+        self.repo: hgclient = hgclient(path=self.repositoryPath.absolute().__str__())
+        super().__init__()
 
-    try:
-        branches.index(branch)
-        logging.info(msg=f"{branch} is a valid Git branch")
-        return True
-    except ValueError:
-        logging.info(msg=f"{branch} is not a valid Git branch")
-        return False
+    def getDefaultBranchName(self) -> str:
+        branch: str = self.repo.branch()
+        logging.info(msg=f"{branch} is the default Mercurial branch")
+        return branch
 
+    def checkIfBranch(self, branch: str) -> bool:
+        detailedBranches: List[Tuple[bytes, int, bytes]] = self.repo.branches(
+            closed=True
+        )
+        branches: List[str] = [branch.decode() for branch, _, _ in detailedBranches]
 
-def restoreRepoToBranch(branch: str, repo: hgclient) -> None:
-    repo.update(rev=branch, clean=True)
-    logging.info(msg=f"Restored repo to {branch} branch")
+        try:
+            branches.index(branch)
+            logging.info(msg=f"{branch} is a valid Git branch")
+            return True
+        except ValueError:
+            logging.info(msg=f"{branch} is not a valid Git branch")
+            return False
 
+    def restoreRepoToBranch(self, branch: str) -> None:
+        self.repo.update(rev=branch, clean=True)
+        logging.info(msg=f"Restored repo to {branch} branch")
 
-def getCommitIterator(
-    branch: str, repo: hgclient
-) -> List[Tuple[bytes, bytes, bytes, bytes, bytes, bytes]]:
-    log = repo.log(branch=branch, removed=True)
-    logging.info(msg=f"Created commit iterator for branch {branch}")
-    return log
+    def getCommitIterator(
+        self, branch: str
+    ) -> List[Tuple[bytes, bytes, bytes, bytes, bytes, bytes]]:
+        log = self.repo.log(branch=branch, removed=True)
+        logging.info(msg=f"Created commit iterator for branch {branch}")
+        return log
 
-
-def getCommitCount(
-    commitIterator: List[Tuple[bytes, bytes, bytes, bytes, bytes, bytes, datetime]]
-) -> int:
-    count: int = len(commitIterator)
-    logging.info(msg=f"Found {count} commits")
-    return count
+    def getCommitCount(
+        commitIterator: List[Tuple[bytes, bytes, bytes, bytes, bytes, bytes, datetime]]
+    ) -> int:
+        count: int = len(commitIterator)
+        logging.info(msg=f"Found {count} commits")
+        return count
